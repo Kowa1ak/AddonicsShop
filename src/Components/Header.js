10cb68794077b13b6417b70d82dc0aeb46a535ff
kowalak <danila.tsatsuk22@gmail.com>
2023-08-15 16:08:26 +0300

    logic for adding items to the cart

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,25 @@
 import React, { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
+import Order from "./Order";
 
-export default function Header() {
+const showOrders = (props) => {
+  return (
+    <div>
+      {" "}
+      {props.orders.map((el) => (
+        <Order key={el.id} item={el} />
+      ))}
+    </div>
+  );
+};
+const showNothing = () => {
+  return (
+    <div className="empty">
+      <h2>Товаров нет</h2>
+    </div>
+  );
+};
+export default function Header(props) {
   let [cartOpen, setCartOpen] = useState(false);
 
   return (
@@ -17,7 +35,11 @@ export default function Header() {
           className={`shop-cart-button ${cartOpen ? "active" : ""}`}
           onClick={() => setCartOpen((cartOpen = !cartOpen))}
         />
-        {cartOpen && <div className="shop-cart"></div>}
+        {cartOpen && (
+          <div className="shop-cart">
+            {props.orders.length > 0 ? showOrders(props) : showNothing()}
+          </div>
+        )}
       </div>
       <div className="presentation"></div>
     </header>
--- src/Components/Items.js
+++ src/Components/Items.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
-import Item from './Item'
+import React, { Component } from "react";
+import Item from "./Item";
 
 export class Items extends Component {
   render() {
     return (
-     <main>
-        {this.props.items.map(el=>(
-          <Item key={el.id} item={el}/>
+      <main>
+        {this.props.items.map((el) => (
+          <Item key={el.id} item={el} onAdd={this.props.onAdd} />
         ))}
-     </main>
-    )
+      </main>
+    );
   }
 }
 
-export default Items
+export default Items;
