0268b3a32c9a7f8a21c4c3393c39d495f8a46ad8
kowalak <danila.tsatsuk22@gmail.com>
2023-08-15 14:30:39 +0300

    product card design

--- src/App.js
+++ src/App.js
@@ -1,57 +1,56 @@
-import React from 'react';
+import React from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import Items from './Components/Items';
-
+import Items from "./Components/Items";
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      items:[
+      items: [
         {
           id: 1,
-          title: 'Предмет1',
-          img: '',
-          desc: 'описание',
-          category: 'mouse',
-          price: '50.00'
+          title: "Предмет1",
+          img: "airpods.jpg",
+          desc: "описание",
+          category: "mouse",
+          price: "50.00",
         },
         {
           id: 2,
-          title: 'Предмет2',
-          img: '',
-          desc: 'описание',
-          category: 'mouse',
-          price: '50.00'
+          title: "Предмет2",
+          img: "keybord.jpg",
+          desc: "описание",
+          category: "mouse",
+          price: "50.00",
         },
         {
           id: 3,
-          title: 'Предмет3',
-          img: '',
-          desc: 'описание',
-          category: 'mouse',
-          price: '50.00'
+          title: "Предмет3",
+          img: "micro.jpg",
+          desc: "описание",
+          category: "mouse",
+          price: "50.00",
         },
         {
           id: 4,
-          title: 'Предмет4',
-          img: '',
-          desc: 'описание',
-          category: 'mouse',
-          price: '50.00'
-        }
-      ]
-    }
+          title: "Предмет4",
+          img: "mouse.jpg",
+          desc: "описание",
+          category: "mouse",
+          price: "50.00",
+        },
+      ],
+    };
   }
-  render(){
-  return (
-    <div className="wrapper">
-      <Header />
-      <Items items={this.state.items}/>
-      <Footer />
-    </div>
-  )
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
+    );
   }
 }
 
